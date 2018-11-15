30b83023a55e00dc2dc6680763bcbb309715a9fa
MrTroble <nifroh56@gmail.com>
2018-11-15 18:00:59 +0100

    Initial commit

---
+++ src/io/github/troblecodings/ctf_server/SSLServer.java
@@ -0,0 +1,44 @@
+package io.github.troblecodings.ctf_server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Date;
+
+import javax.net.ssl.*;
+
+/*-*****************************************************************************
+ * Copyright 2018 MrTroble
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+
+/**
+ * @author MrTroble
+ *
+ */
+public class SSLServer {
+	
+	public static LoggerFile LOGGER;
+	private static SSLServerSocket sslserver;
+	
+	public static void main(String[] args) throws Exception {
+		Date date = new Date();
+		LOGGER = new LoggerFile(new FileOutputStream(new File("log-" + date.getMonth() + "-" + date.getDay() + "-" + (1900 + date.getYear()))));
+		sslserver = (SSLServerSocket) SSLServerSocketFactory.getDefault().createServerSocket(555);
+		while(true) {
+			Socket sk = sslserver.accept();
+			LOGGER.println("Client[" + sk + "] connected to server");
+		}
+	}
+
+}
